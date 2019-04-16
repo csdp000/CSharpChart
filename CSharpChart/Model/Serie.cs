c5c95d80711dd0472b0e651caaa2b9db4ac80bab
2ykwang <csdp000@outlook.com>
2019-04-16 18:30:56 +0900

    초기 커밋.

---
+++ CSharpChart/Model/Serie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CSharpChart.Model
+{
+    public class Serie
+    {
+        public string Text { get; set; }
+        public float Value { get; set; }
+        public Color LineColor { get; set; }
+
+
+        public Serie(string text) : this(text, 0, Color.Black) { }
+        public Serie(string text, Color color) : this(text, 0, color) { }
+        public Serie(string text, float value) : this(text, value, Color.Black) { }
+        public Serie(string text, float value, Color color)
+        {
+            LineColor = color;
+            Value = value;
+            Text = text;
+        }
+    }
+}
